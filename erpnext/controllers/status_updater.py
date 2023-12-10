143d1401521ac48a6d3ca3a87ab6383e42009c6e
flo0o0o0 <florian-binder@outlook.dem>
2023-12-10 15:27:51 +0000

    uncomment validation for  negative qty in delivery note items // needed to accept package returns

--- erpnext/controllers/status_updater.py
+++ erpnext/controllers/status_updater.py
@@ -227,8 +227,8 @@ class StatusUpdater(Document):
 
 			# get unique transactions to update
 			for d in self.get_all_children():
-				if hasattr(d, "qty") and d.qty < 0 and not self.get("is_return"):
-					frappe.throw(_("For an item {0}, quantity must be positive number").format(d.item_code))
+				#if hasattr(d, "qty") and d.qty < 0 and not self.get("is_return"):
+				#	frappe.throw(_("For an item {0}, quantity must be positive number").format(d.item_code))
 
 				if hasattr(d, "qty") and d.qty > 0 and self.get("is_return"):
 					frappe.throw(_("For an item {0}, quantity must be negative number").format(d.item_code))
